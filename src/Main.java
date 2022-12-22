f4ea086760f08524adf9b2260bf205df06596c20
KisEA <maslennikvakat@mail.ru>
2022-12-23 01:16:33 +0300

    Исправлено форматирование

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
                 new int[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
                 new int[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
         };
-        for(int i = 0; i < grid.length; i++) {
+        for (int i = 0; i < grid.length; i++) {
             System.out.println(Arrays.toString(grid[i]));
         }
         int maxIsland = Solution.maxAreaOfIsland(grid);
@@ -24,13 +24,13 @@ public class Main {
                 new int[]{1, 1, 1},
                 new int[]{1, 1, 0},
                 new int[]{1, 0, 1}
-                };
-        for(int i = 0; i < col.length; i++) {
+        };
+        for (int i = 0; i < col.length; i++) {
             System.out.println(Arrays.toString(col[i]));
         }
         System.out.println();
         col = Solution2.floodFill(col, 1, 1, 2);
-        for(int i = 0; i < col.length; i++) {
+        for (int i = 0; i < col.length; i++) {
             System.out.println(Arrays.toString(col[i]));
         }
         System.out.println();
--- src/Solution3.java
+++ src/Solution3.java
@@ -2,14 +2,15 @@ class Solution3 {
     public static boolean isSymmetric(TreeNode root) {
         return symTree(root.left, root.right);
     }
-    private static boolean symTree(TreeNode p, TreeNode q){
-        if (p == null && q == null){
+
+    private static boolean symTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
             return true;
         }
-        if (p == null || q == null){
+        if (p == null || q == null) {
             return false;
         }
-        if (p.val != q.val){
+        if (p.val != q.val) {
             return false;
         }
         return symTree(p.left, q.right) && symTree(p.right, q.left);
